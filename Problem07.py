f7b388dd852883762310b9936c85c12c743fa07c
Muhammed Salah Uddion <168917530+SalahuddinWebDevloper@users.noreply.github.com>
2025-02-03 01:58:00 -0500

    Add files via upload
    Write a python program to find the largest number between two numbers using function

---
+++ Problem07.py
@@ -0,0 +1,11 @@
+def find_largest(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+largest = find_largest(num1, num2)
+
+print("The largest number is:", largest)
